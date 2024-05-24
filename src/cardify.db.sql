d50b458b04a677477767449bea8790a8be4d77a5
John Paul Ayop <jpayop1@gmail.com>
2024-05-24 14:57:43 +0700

    - K, added sql queries

--- src/cardify.db.sql
+++ src/cardify.db.sql
@@ -9,3 +9,6 @@ CREATE TABLE deck(
 CREATE TABLE card(
     
 );
+
+CREATE TABLE `cardifydb`.`user` (`user_id` INT(7) NOT NULL AUTO_INCREMENT , `user_username` VARCHAR(40) NOT NULL , `user_password` TEXT NOT NULL , `user_email` VARCHAR(50) NOT NULL , PRIMARY KEY (`user_id`)) ENGINE = InnoDB;
+
